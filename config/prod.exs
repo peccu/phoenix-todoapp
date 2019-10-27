196d4ad77c29e3b7c77a070b48f33fd2257ac24b
peccu <peccul@gmail.com>
2019-10-27 12:30:52 +0900

    add gigalixir configuration

--- config/prod.exs
+++ config/prod.exs
@@ -16,6 +16,23 @@ config :my_app, MyAppWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
+## Gigalixir config
+# https://gigalixir.readthedocs.io/en/latest/main.html#modifying-existing-app-with-mix
+# verify
+# APP_NAME=foo SECRET_KEY_BASE="$(mix phx.gen.secret)" MIX_ENV=prod DATABASE_URL="postgresql://user:pass@localhost:5432/foo" PORT=4000 mix phx.server
+# curl localhost:4000
+config :my_app, MyAppWeb.Endpoint,
+  http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 80],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
+
+config :my_app, MyApp.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  ssl: true,
+  pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
+
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
