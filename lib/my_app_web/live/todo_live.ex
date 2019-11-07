73dcd5f0aa6efc22e01eae7456c94c131d42aa50
peccu <peccul@gmail.com>
2019-11-08 00:41:31 +0900

    add todo list showing

--- lib/my_app_web/live/todo_live.ex
+++ lib/my_app_web/live/todo_live.ex
@@ -1,18 +1,22 @@
 defmodule MyAppWeb.TodoLive do
   use Phoenix.LiveView
   import Calendar.Strftime
+  alias MyApp.Todos
 
   def render(assigns) do
     ~L"""
     <div>
       <h2><%= @salutation %></h2>
       <h2>It's <%= strftime!(@date, "%r") %></h2>
+      <%= for todo <- @todos do %>
+      <div><%= todo.title %></div>
+      <% end %>
     </div>
     """
   end
 
   def mount(_session, socket) do
-    if connected?(socket), do: :timer.send_interval(1000, self(), :tick)
+    # if connected?(socket), do: :timer.send_interval(1000, self(), :tick)
 
     {:ok, put_date(socket)}
   end
@@ -23,6 +27,9 @@ defmodule MyAppWeb.TodoLive do
 
   defp put_date(socket) do
     salutation = "Welcome to LiveView, from the Programming Phoenix team!"
-    assign(socket, salutation: salutation, date: :calendar.local_time())
+    assign(socket,
+      salutation: salutation,
+      date: :calendar.local_time(),
+      todos: Todos.list_todos())
   end
 end
