bb1d807d9922bc14f58bb3783dfc8dfa5e3a1e04
peccu <peccul@gmail.com>
2019-11-11 16:43:37 +0900

    add check_origin

--- config/prod.exs
+++ config/prod.exs
@@ -11,6 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :app, AppWeb.Endpoint,
   # url: [host: "example.com", port: 80],
+  check_origin: ["//0.0.0.0:4000"],
   server: true,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
