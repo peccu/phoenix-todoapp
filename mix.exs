a1997bfcff1748e27ac4ee21a76b57c589dcdaf6
peccu <peccul@gmail.com>
2019-11-11 11:55:58 +0900

    update phoenix and phoenix_ecto and phoenix_live_view

--- mix.exs
+++ mix.exs
@@ -33,14 +33,14 @@ defmodule App.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.10"},
+      {:phoenix, "~> 1.4.11"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.1"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.3.1"},
+      {:phoenix_live_view, "~> 0.4.1"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
