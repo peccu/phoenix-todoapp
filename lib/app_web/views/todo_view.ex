7413097d7ad49a9cf0ada9388d41c38b2b6ca813
peccu <peccul@gmail.com>
2019-12-05 16:48:35 +0900

    fix style by formatter

--- lib/app_web/views/todo_view.ex
+++ lib/app_web/views/todo_view.ex
@@ -5,15 +5,19 @@ defmodule AppWeb.TodoView do
   @url_pattern ~r/(^|[\s\n]|<[A-Za-z]*\/?>)((?:https?|ftp):\/\/[\-A-Za-z0-9+@#\/%?=()~_|!:,.;]*[\-A-Za-z0-9+@#\/%=~()_|])/
 
   def url_to_safelink(_all, prev, url) do
-    link = Phoenix.HTML.Link.link(url, to: url, target: "_blank")
-    |> Phoenix.HTML.safe_to_string()
+    link =
+      Phoenix.HTML.Link.link(url, to: url, target: "_blank")
+      |> Phoenix.HTML.safe_to_string()
+
     prev <> link
   end
 
   def autolinker(text) do
-    safe_text = text
-    |> Phoenix.HTML.html_escape()
-    |> Phoenix.HTML.safe_to_string()
+    safe_text =
+      text
+      |> Phoenix.HTML.html_escape()
+      |> Phoenix.HTML.safe_to_string()
+
     @url_pattern
     |> Regex.replace(safe_text, &url_to_safelink/3)
   end
