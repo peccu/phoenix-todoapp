4a6a9f2572bbd36e5bee9c3a4c8d19847fa29548
peccu <peccul@gmail.com>
2019-12-20 14:24:57 +0900

    move view and etc to live view like component

--- lib/app_web/live/todo_live.ex
+++ lib/app_web/live/todo_live.ex
@@ -1,11 +1,81 @@
 defmodule AppWeb.TodoLive do
   use Phoenix.LiveView
+  use Phoenix.HTML
   alias App.Todos
-  alias AppWeb.TodoView
   require Logger
 
+  # pattern is from https://github.com/bryanwoods/autolink-js/blob/master/autolink.js
+  @url_pattern ~r/(^|[\s\n]|<[A-Za-z]*\/?>)((?:https?|ftp):\/\/[\-A-Za-z0-9+@#\/%?=()~_|!:,.;]*[\-A-Za-z0-9+@#\/%=~()_|])/
+
+  def url_to_safelink(_all, prev, url) do
+    link =
+      Phoenix.HTML.Link.link(url, to: url, target: "_blank")
+      |> Phoenix.HTML.safe_to_string()
+
+    prev <> link
+  end
+
+  def autolinker(text) do
+    safe_text =
+      text
+      |> Phoenix.HTML.html_escape()
+      |> Phoenix.HTML.safe_to_string()
+
+    @url_pattern
+    |> Regex.replace(safe_text, &url_to_safelink/3)
+  end
+
   def render(assigns) do
-    TodoView.render("todos.html", assigns)
+    ~L"""
+      <div>
+      <%= if @show_done do %>
+        <center><button phx-click="hide_done">Hide Done</button></center>
+      <% else %>
+        <center><button phx-click="show_done">Show Done</button></center>
+      <% end %>
+      </div>
+      <table>
+        <tr>
+          <th>Done</th>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Action</th>
+        </tr>
+      <%= for todo <- @todos do %>
+        <%= if @show_done || not todo.done do %>
+        <tr>
+          <td><%= checkbox(:todo, :done, phx_click: "toggle_done", phx_value_id: todo.id, value: todo.done) %></td>
+          <td><%= todo.id %></td>
+          <%= if @in_edit != todo.id do %>
+          <td><%= todo.title |> autolinker |> raw %></td>
+          <td>
+            <center>
+              <%= if @in_edits |> Enum.member?(todo.id) do %>
+              <button phx-click="start_edit" phx-value-id="<%= todo.id %>" disabled>Someone is Editing</button>
+              <% else %>
+              <button phx-click="start_edit" phx-value-id="<%= todo.id %>">Edit</button>
+              <% end %>
+            </center>
+          </td>
+          <% else %>
+          <td colspan=2>
+            <form action="#" phx-submit="save">
+              <%= hidden_input :todo, :id, value: todo.id %>
+              <%= text_input :todo, :title, value: todo.title %>
+              <%= submit "Save", phx_disable_with: "Saving..." %>
+              <button type="cancel" onclick="return false;" phx-click="discard" phx-value-id="<%= todo.id %>">Discard</button>
+            </form>
+          </td>
+          <% end %>
+        </tr>
+        <% end %>
+      <% end %>
+      </table>
+      <form action="#" phx-submit="add">
+        <%= text_input :todo, :title, placeholder: "What do you want to get done?" %>
+        <%= submit "Add", phx_disable_with: "Adding..." %>
+      </form>
+    """
   end
 
   def mount(_session, socket) do
