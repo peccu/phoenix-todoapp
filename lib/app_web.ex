d3463601e0148e8a5b73fcc289584c3a577d7d59
peccu <peccul@gmail.com>
2020-01-08 09:41:03 +0900

    fix format

--- lib/app_web.ex
+++ lib/app_web.ex
@@ -43,6 +43,7 @@ defmodule AppWeb do
       import AppWeb.ErrorHelpers
       import AppWeb.Gettext
       alias AppWeb.Router.Helpers, as: Routes
+
       import Phoenix.LiveView,
         only: [live_render: 2, live_render: 3, live_link: 1, live_link: 2]
     end
